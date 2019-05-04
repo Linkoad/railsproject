43e171677c656ee23d47ab5381fee9f2e7883061
Linkoad <danteabreu1@gmail.com>
2019-05-04 00:57:52 -0300

    Add likes and unlikes

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,25 @@ Rails.application.routes.draw do
 	constraints Clearance::Constraints::SignedIn.new do
 		root to: 'dashboards#show'
 	end
-  root to: 'homes#show'
-  resources :shouts, only: [:create, :show]
-  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
-  resource :session, only: [:create]
+	root to: 'homes#show'
+	resources :shouts, only: [:create, :show] do
+		member do
+			post "like" => "likes#create"
+			delete "unlike" => "likes#destroy"
+		end
+	end
 
-  resources :users, only: [:create, :show] do
-    resource :password,
-      controller: "clearance/passwords",
-      only: [:create, :edit, :update]
-  end
+	resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+	resource :session, only: [:create]
 
-  get "/sign_in" => "sessions#new", as: "sign_in"
-  delete "/sign_out" => "sessions#destroy", as: "sign_out"
-  get "/sign_up" => "users#new", as: "sign_up"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :users, only: [:create, :show] do
+		resource :password,
+			controller: "clearance/passwords",
+			only: [:create, :edit, :update]
+	end
+
+	get "/sign_in" => "sessions#new", as: "sign_in"
+	delete "/sign_out" => "sessions#destroy", as: "sign_out"
+	get "/sign_up" => "users#new", as: "sign_up"
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_115954) do
+ActiveRecord::Schema.define(version: 2019_05_03_150524) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "shout_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shout_id"], name: "index_likes_on_shout_id"
+    t.index ["user_id", "shout_id"], name: "index_likes_on_user_id_and_shout_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "photo_shouts", force: :cascade do |t|
     t.string "image_file_name"
