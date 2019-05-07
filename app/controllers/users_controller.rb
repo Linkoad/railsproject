7ddc61aaea7f9ab991227cd3c32220b8c717abc0
Linkoad <danteabreu1@gmail.com>
2019-05-07 23:59:31 +0000

    Add validades-users-views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,6 @@ class UsersController < Clearance::UsersController
 	private
 
 	def user_params
-		params.require(:user).permit(:name, :email, :password)
+		params.require(:user).permit(:username, :email, :password)
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   include Clearance::User
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
   
   has_many :shouts, dependent: :destroy
   has_many :likes
