a8747d16009effc61a634868b7f869b58bd225c4
Linkoad <danteabreu1@gmail.com>
2019-05-09 17:47:43 +0000

    Add follow-users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,17 @@ class User < ApplicationRecord
   has_many :likes
   has_many :liked_shouts, through: :likes, source: :shout
   
+  has_many :following_relationships, foreign_key: :follower_id
+  has_many :followed_users, through: :following_relationships
+    
+  def follow(user)
+    followed_users << user
+  end
+  
+  def following?(user)
+    followed_user_ids.include?(user.id)
+  end
+  
   def like(shout)
 	  liked_shouts << shout
   end
@@ -18,9 +29,8 @@ class User < ApplicationRecord
   def liked?(shout)
 	  liked_shout_ids.include?(shout.id)
   end
-
+  
   def to_param
 	  username
   end
 end
-
